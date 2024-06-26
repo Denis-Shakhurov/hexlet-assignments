71a1871d2fbbccfbc4d9f6b21e2ff2db1c005487
@hexlet/cli <support@hexlet.io>
2024-06-26 08:53:45 +0700

    submit java-oop-ru/classes-and-objects

--- java-oop-ru/classes-and-objects/src/main/java/exercise/Point.java
+++ java-oop-ru/classes-and-objects/src/main/java/exercise/Point.java
@@ -1,13 +1,21 @@
 package exercise;
 
 // BEGIN
-import lombok.Getter;
-import lombok.AllArgsConstructor;
-
-@Getter
-@AllArgsConstructor
 public class Point {
   private int x;
   private int y;
+
+  public Point(int x, int y) {
+      this.x = x;
+      this.y = y;
+  }
+
+  public int getX() {
+      return x;
+  }
+
+  public int getY() {
+      return y;
+  }
 }
 // END
