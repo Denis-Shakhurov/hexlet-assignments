f94235651d571953196fce1f3f74967bbd6586c1
@hexlet/cli <support@hexlet.io>
2024-09-21 09:41:31 +0700

    submit java-advanced-ru/multithreading-spring

--- java-advanced-ru/multithreading-spring/src/main/java/exercise/service/UserService.java
+++ java-advanced-ru/multithreading-spring/src/main/java/exercise/service/UserService.java
@@ -31,9 +31,12 @@ public class UserService {
     public Mono<User> update(User user, Long id) {
         return userRepository.findById(id)
                 .flatMap(u -> {
-                    user.setId(u.getId());
+                    user.setId(id);
                     return userRepository.save(user);
                 });
+        //var userUpdate = new User(user.getFirstName(), user.getLastName(), user.getEmail());
+        //userUpdate.setId(id);
+        //return userRepository.save(userUpdate);
     }
 
     public Mono<Void> delete(Long id) {
